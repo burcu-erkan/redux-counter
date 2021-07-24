5eb52bb3486b90dee8c924d9f5983c5cf7c29e3d
burcu <burcu@MacBook-Air.local>
2021-07-24 16:55:10 -0700

    done counter with react-redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "./actions/index";
 
 function App() {
+  const counter = useSelector((state) => state.counterReducer);
+  const isLogged = useSelector((state) => state.loggedReducer);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <h1 className={counter > 0 ? "poz" : counter < 0 ? "neg" : null}>
+        {" "}
+        {counter}
+      </h1>
+      <div className="buttons">
+        <button onClick={() => dispatch(increment(1))}>Increment</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(decrement(1))}>Decrement</button>
+      </div>
+
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ""}
     </div>
   );
 }
